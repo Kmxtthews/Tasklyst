766bb6bfaf3860ce652aca2b76901d2bcce1f84f
SaladAss <131820783+Kmxtthews@users.noreply.github.com>
2024-12-20 18:14:16 +0000

    Add files via upload

---
+++ TaskManager/TaskManager/formDeadlines.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager
+{
+    public partial class formDeadlines : Form
+    {
+        public formDeadlines()
+        {
+            InitializeComponent();
+        }
+
+        private void formDeadlines_Load(object sender, EventArgs e)
+        {
+            // Removes the border of the form
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+    }
+}
